0d4a1f1133d05c58ef892242078ea160b19cba78
Vivekanand P V <vivekanandpv@outlook.com>
2019-11-20 17:08:17 +0530

    Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ParentComponent, ChildComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+  selector: "app-child",
+  templateUrl: "./child.component.html",
+  styleUrls: ["./child.component.css"]
+})
+export class ChildComponent implements OnInit {
+  @Input() childTitle: string;
+  @Input() childDate: Date;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
