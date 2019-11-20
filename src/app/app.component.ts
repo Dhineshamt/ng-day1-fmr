43d76f130862a6c80edbf759a70f0f8bc7c3efd2
Vivekanand P V <vivekanandpv@outlook.com>
2019-11-20 15:16:09 +0530

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,8 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  amount = 100.25;
+  today = new Date();
+  title = "Hi there!";
+}
