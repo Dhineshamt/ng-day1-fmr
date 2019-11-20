4f607258939dd86787c9104e168d352975086629
Vivekanand P V <vivekanandpv@outlook.com>
2019-11-20 16:24:43 +0530

    Starting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,4 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  amount = 100.25;
-  today = new Date();
-  title = "Hi there!";
-}
+export class AppComponent {}
--- src/app/my-currency.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyCurrencyPipe } from './my-currency.pipe';
-
-describe('MyCurrencyPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MyCurrencyPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
